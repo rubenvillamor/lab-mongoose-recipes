e6b7d769d4c605c0685b12711c289be49201e242
Ruben <villamorlage@gmail.com>
2023-08-03 19:53:45 +0200

    avance

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data.json");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose.connect('mongodb://localhost:27017/recipe-app')
+mongoose.connect("mongodb://127.0.0.1:27017/recipe-app");
+
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Model.create({
-      
-
-    })
     return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Lubina al horno",
+      level: "Amateur Chef",
+      ingredients: [
+        "Lubina salvaje",
+        "Patatas",
+        "Cebolla",
+        "Ajo",
+        "Aceite",
+        "Vinagre",
+      ],
+      cuisine: "Mediterranea",
+      thisType: "main_course",
+      image:
+        "https://i.blogs.es/d40988/lubina-al-horno-mas-facil-del-mundo-pakus-directo-paladar-2-/1366_2000.jpg",
+      duration: 45,
+      creator: "Uriel",
+    })
+    .then((response)=>{
+      console.log(response.title)
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.insertMany(data).then((response) => {
+      response.forEach((eachRecipe) => {
+        console.log(eachRecipe.title);
+      });
+    });
+    // console.log(response)
+    //
+  })
+
+  // //Read
+  //  // console.log(data)
+  // return Recipe
+  // .find({level: {"Amateur Chef"}})
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
